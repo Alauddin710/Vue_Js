d369c5cc9f4f3387a9b0498aa64b32573199cadb
Alauddinalo710 <mdalauddinalo710@gmail.com>
2023-02-10 19:14:10 +0600

    first_install_vueJs

---
+++ Home_Project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 v-bind="title">{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "well come to vue js",
+    };
+  },
+};
+</script>
+
+<style></style>
