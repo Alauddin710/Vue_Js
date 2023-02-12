e47983463c500351365758a8eb1ef69b59dc193a
Alauddin710 <mdalauddinalo710@gmail.com>
2023-02-12 19:17:14 +0600

    Routing_data_show_axios

--- Class/Project-2/src/main.js
+++ Class/Project-2/src/main.js
@@ -1,5 +1,31 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from "vue"; //CreateApp
+import App from "./App.vue"; //main component
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./pages/Home.vue";
+import About from "./pages/About.vue";
+import Contact from "./pages/Contact.vue";
+import News from "./pages/News.vue";
+import Products from "./pages/Products.vue";
+import Search from "./pages/Search.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+  { path: "/contact", component: Contact },
+  { path: "/news", component: News },
+  { path: "/products", component: Products },
+  { path: "/search", component: Search },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  linkActiveClass: "active",
+});
+
+createApp(App).use(router).mount("#app");
+
+//****** */
 // import App from "./App1.vue";
 // import App from "./Reactive.vue";
 // import App from "./Reactive1.vue";
@@ -12,5 +38,4 @@ import App from "./App.vue";
 // import App from "./Event1.vue";
 // import App from "./Event2.vue";
 // import App from "./components/Form1.vue";
-
-createApp(App).mount("#app");
+//**** */
---
+++ Class/Project-2/src/pages/Search.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-sm-12">
+        <h2>Search Item</h2>
+        <form v-on:submit.prevent="search()" method="post">
+          <input type="text" v-model="searchItem" />
+          <button type="submit">Search</button>
+        </form>
+        {{ searchItem }}
+        <hr />
+        <div v-for="post in posts" :key="post.id">
+          <h2>{{ post.product_name }}</h2>
+          <div class="fakeimg">Fake Image</div>
+          <p>
+            {{ post.product_details }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      posts: [],
+      searchItem: "",
+      searchResult: [],
+    };
+  },
+  methods: {
+    search() {
+      axios
+        .post("http://127.0.0.1:8000/api/search", { item: this.searchItem })
+        .then((response) => {
+          // this.posts = response.data;
+          console.log(response.data);
+        });
+    },
+  },
+};
+</script>
+<style></style>
