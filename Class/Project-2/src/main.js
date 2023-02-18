9625847faef3e5b48a3c4ca81409969d25d56c17
Alauddin710 <mdalauddinalo710@gmail.com>
2023-02-18 18:51:03 +0600

    vuejs_some_work

--- Class/Project-2/src/main.js
+++ Class/Project-2/src/main.js
@@ -7,7 +7,8 @@ import Contact from "./pages/Contact.vue";
 import News from "./pages/News.vue";
 import Products from "./pages/Products.vue";
 import Search from "./pages/Search.vue";
-
+import { createStore } from "vuex";
+import Cart from "./pages/Cart.vue";
 const routes = [
   { path: "/", component: Home },
   { path: "/about", component: About },
@@ -15,6 +16,7 @@ const routes = [
   { path: "/news", component: News },
   { path: "/products", component: Products },
   { path: "/search", component: Search },
+  { path: "/cart", component: Cart },
 ];
 
 const router = createRouter({
@@ -23,7 +25,37 @@ const router = createRouter({
   linkActiveClass: "active",
 });
 
-createApp(App).use(router).mount("#app");
+// configure the store
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    increment(state, payLoad) {
+      state.counter = state.counter + payLoad;
+    },
+  },
+  getters: {
+    getCounter(state) {
+      return state.counter;
+    },
+    getNormalizedCounter(state, getter) {
+      // get the getCounter() return
+      // value from the object
+      if (getter.getCounter >= 50) {
+        return 50;
+      }
+      return getter.getCounter;
+    },
+  },
+});
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount("#app");
 
 //****** */
 // import App from "./App1.vue";
---
+++ Class/Project-2/src/pages/Cart.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-sm-8">
+        <h2>Shoping Cart</h2>
+
+        <p>Counter: {{ $store.getters.getCounter }}</p>
+        <p>Normalized: {{ $store.getters.getNormalizedCounter }}</p>
+        <h1>{{ $store.state.counter }}</h1>
+        <button @click="$store.commit('increment', 10)">Clic Here</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
